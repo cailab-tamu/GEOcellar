8ab0c6dc44dd69bd76766f0d1f03971f16c35a34
James Cai <jamescai@genomezoo.net>
2025-07-23 11:28:26 -0500

    Add SQLite database tools and browser GUI
    Added SQLite database files, JDBC driver, and MATLAB mex interface for SQLite. Introduced a new MATLAB GUI (SQLiteDatabaseBrowser_java.m) for browsing and querying SQLite databases. Updated test_sqlite_query.m to demonstrate multiple SQLite access methods. Added reference URLs and documentation for related tools.

--- toolbox/+dbs/test_sqlite_query.m
+++ toolbox/+dbs/test_sqlite_query.m
@@ -1,4 +1,6 @@
-db = sqlite('geo.db');
+import dbs.*
+
+db = sqlite('+dbs\geo.db');
 
 % Query
 seriesData = fetch(db, 'SELECT * FROM series');
@@ -6,8 +8,24 @@ sampleData = fetch(db, 'SELECT * FROM samples');
 disp(seriesData)
 disp(sampleData)
 
+%query = sprintf("PRAGMA table_info(%s)", "samples");
+%schemaResult = fetch(db, query);
+close(db)
+
+
+% https://github.com/a-ma72/mksqlite
+dbid = mksqlite('open', '+dbs\geo.db');
+query = sprintf("PRAGMA table_info(%s)", "samples");
+query = 'SELECT * FROM series';
+schemaResult = mksqlite(dbid, query)
+mksqlite(dbid, 'close')
+
+%%
+
+javaaddpath('+dbs\sqlite-jdbc-3.50.3.0.jar');
+url = 'jdbc:sqlite:+dbs\geo.db';
+conn = database('', '', '', 'org.sqlite.JDBC', url);
+data = fetch(conn, 'SELECT * FROM series');
 
 query = sprintf("PRAGMA table_info(%s)", "samples");
-schemaResult = fetch(db, query);
-
-close(db)
+data = fetch(conn, query)
